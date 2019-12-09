165f779a451d2ee18572805f4943c67938d7b712
Ari Makela <ari.makela@iki.fi>
2019-12-10 00:18:45 +0200

    serializing single object works

--- mkjson.py
+++ mkjson.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import json
 import logging
 
@@ -7,14 +8,33 @@ import openingclassification
 
 logging.basicConfig(level=logging.DEBUG)
 
-class ClassificationItem(object):
+
+class ClassificationItem(json.JSONEncoder):
     eco = ''
     nic = ''
     name = ''
     fen = ''
     moves = ''
 
+    def __init__(self, fen=fen, eco=eco, name=name, nic=nic):
+        self.fen = fen
+        self.eco = eco
+        self.name = name
+        self.nic = nic
+
+    # def default(self, o):
+    #     logging.debug("default o={0}".format(o))
+    #     return json.JSONEncoder.default(self, o)
+
+    # def encode(self, o):
+    #     logging.debug("encode o={0]".format(o))
+
+    # def iterencode(self, o):
+    #     logging.debug("iterencode o={0]".format(o))
+
+
 def main():
+    #items = collections.OrderedDict()
     items = dict()
     for k, v in openingclassification.NIC_DATA.items():
         item = ClassificationItem()
@@ -39,7 +59,17 @@ def main():
             items[k] = item
 
     # this does not work, see https://stackoverflow.com/questions/10252010/serializing-python-object-instance-to-json
+    # http://www.marcstober.com/blog/2007/07/07/serializing-arbitrary-python-objects-to-json-using-__dict__/
     # json.dumps(items)
+    # This works, but does not preserve the order of the properties
+    # print(json.dumps([item.__dict__ for item in items.values()]))
+    # print(json.dumps(items))
+    fen = 'rnbqkbnr/ppp2ppp/8/3pp3/4PP2/8/PPPP2PP/RNBQKBNR w KQkq -'
+    logging.debug("FEN=%s NIC=%s" % (fen, items[fen].nic))
+    print(json.dumps(
+        items[fen].__dict__
+    ))
+
 
 if __name__ == '__main__':
     main()
